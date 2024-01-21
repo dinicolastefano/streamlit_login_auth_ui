6daf8b13408cc07e4c27ad3f422170cdd41126e9
dinicolastefano <122374188+dinicolastefano@users.noreply.github.com>
2024-01-22 00:02:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from streamlit_login_auth_ui.widgets import __login__
 
-__login__obj = __login__(auth_token = "courier_auth_token",
+__login__obj = __login__(auth_token = "pk_prod_RKE7Q0BBDKMTDTKJVG15EKPXFXT4",
                     company_name = "Shims",
                     width = 200, height = 250,
                     logout_button_name = 'Logout', hide_menu_bool = False,
